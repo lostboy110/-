3c12233572e2901a3a9bb9011b18785dab583284
lostboy110 <413737761@qq.com>
2020-08-03 19:21:38 +0800

    8.3固定界面大小，新增复制功能

--- 百度翻译/main.py
+++ 百度翻译/main.py
@@ -1,59 +1,77 @@
 import sys
+
 from PyQt5.QtWidgets import QApplication, QMainWindow
-import Ui_baidu
-from functools import partial
+
+#import Ui_baidu
+from Ui_baidu import Ui_MainWindow
+#from functools import partial
 
 import requests
 import json
 import time
 
 
-def fanyi(ui):
 
-    url= 'https://fanyi.baidu.com/multitransapi'
+class baidufanyi(QMainWindow,Ui_MainWindow):
 
-    headers={
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36'
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        
+        self.pushButton.clicked.connect(self.fanyi)
+        self.pushButton_2.clicked.connect(self.fuzhi)
+
+#实例化剪切板
+        self.clipboard = QApplication.clipboard()  
+
+
+    def fanyi(self):
+
+        url= 'https://fanyi.baidu.com/multitransapi'
+
+        headers={
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36'
+            }
+
+        #ui.textEdit.clear()
+
+        input = self.textEdit.toPlainText()
+        coent = input
+
+        data = {
+            'from': 'en',
+            'to': 'zh',
+            'query': coent,
+            'source': 'txt'
         }
 
-    #ui.textEdit.clear()
+        respos = requests.post(url,data,headers=headers).json()
 
-    input = ui.textEdit.toPlainText()
-    coent = input
+        result = (respos['data']['cands'][0])
 
-    data = {
-        'from': 'en',
-        'to': 'zh',
-        'query': coent,
-        'source': 'txt'
-    }
-
-    respos = requests.post(url,data,headers=headers).json()
-
-    result = (respos['data']['cands'][0])
-
-    ui.textEdit_2.setPlainText(str(result))
-    
-
-def qk(ui):
-    ui.textEdit.clear()
+        self.textEdit_2.setPlainText(str(result))
 
 
- #array5 = array1.copy() # 对原始的array1的复制
+
+#复制功能实现
+    def fuzhi (self):
+        self.clipboard.setText(self.textEdit_2.toPlainText())
+
 
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     MainWindow = QMainWindow()
-    ui = Ui_baidu.Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
 
-    ui.pushButton.clicked.connect(partial(fanyi, ui))
+    myWin = baidufanyi()
+    myWin.show()
 
 
     sys.exit(app.exec_())
+
+
 
 
 
